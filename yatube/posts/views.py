4829eb78bf1ce215862a0ce3dddcecf5531ac54b
Арсений Нариков <narikov02@list.ru>
2021-10-31 16:00:38 +0300

    First commit:tests

---
+++ yatube/posts/views.py
@@ -0,0 +1,96 @@
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Post, Group, User
+from django.core.paginator import Paginator
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    post_list = Post.objects.all()
+    # Если порядок сортировки определен в классе Meta модели,
+    # запрос будет выглядить так:
+    # post_list = Post.objects.all()
+    # Показывать по 10 записей на странице.
+    paginator = Paginator(post_list, 10)
+
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
+    # Отдаем в словаре контекста
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    post_list = group.posts.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'group': group,
+        'page_obj': page_obj,
+    }
+    template = 'posts/group_list.html'
+    return render(request, template, context)
+
+
+def profile(request, username):
+    # Здесь код запроса к модели и создание словаря контекста
+    user = get_object_or_404(User, username=username)
+    post_list = user.posts.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    num = post_list.count()
+    # Отдаем в словаре контекста
+    context = {
+        "page_obj": page_obj,
+        "num": num,
+        "name": user,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    # Здесь код запроса к модели и создание словаря контекста
+    post = get_object_or_404(Post, pk=post_id)
+    author_all_posts = post.author.posts.all()
+    num_of_posts = author_all_posts.count()
+    text = post.text[:30]
+    context = {
+        "post": post,
+        "text": text,
+        "num": num_of_posts,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    username = request.user.username
+    form = PostForm(request.POST)
+    if form.is_valid() and request.method == 'POST':
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', username=username)
+    return render(request, 'posts/post_create.html',
+                           {'form': form, 'is_edit': False})
+
+
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.user != post.author:
+        return redirect('posts:post_detail', post_id=post_id)
+    form = PostForm(request.POST, instance=post)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id=post_id)
+    return render(request, 'posts/post_create.html',
+                           {'form': form, 'is_edit': True,
+                            'post_id': post_id})
